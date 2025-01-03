ae43a813816f06f0cd4fddbda0823c45808496df
Ross Mackay <rossm11781@gmail.com>
2025-01-03 13:58:53 +0000

    test2

--- update/update.py
+++ update/update.py
@@ -7,9 +7,10 @@ def git_pull():
     try:
         # Get the directory of the current script
         script_dir = os.path.dirname(os.path.abspath(__file__))
+        script_dir = os.path.dirname(script_dir)
         
         # Change to the script's directory
-        os.chdir(script_dir+"/..")
+        os.chdir(script_dir)
         
         # Ensure the directory is a git repository
         if not os.path.exists(os.path.join(script_dir, '.git')):
