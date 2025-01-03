95c76afc712fe533af5619c05da4a70ac651b2eb
Ross Mackay <rossm11781@gmail.com>
2025-01-03 13:54:18 +0000

    idiot

--- update/update.py
+++ update/update.py
@@ -9,7 +9,7 @@ def git_pull():
         script_dir = os.path.dirname(os.path.abspath(__file__))
         
         # Change to the script's directory
-        os.chdir(script_dir)
+        os.chdir(script_dir+"/..")
         
         # Ensure the directory is a git repository
         if not os.path.exists(os.path.join(script_dir, '.git')):
