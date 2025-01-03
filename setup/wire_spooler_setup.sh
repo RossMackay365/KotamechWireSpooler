c44da5f6cc61340d9fe89b3500dca97f01aba712
RossMackay365 <157022743+RossMackay365@users.noreply.github.com>
2025-01-03 13:58:00 +0100

    Update wire_spooler_setup.sh

--- setup/wire_spooler_setup.sh
+++ setup/wire_spooler_setup.sh
@@ -1,3 +1,3 @@
-chmod +x home/pi/KotamechWireSpooler/program.py
-ln -s home/pi/KotamechWireSpooler/setup/wire_spooler.service /etc/systemd/system/wire_spooler.service
+chmod +x /home/pi/KotamechWireSpooler/program.py
+ln -s /home/pi/KotamechWireSpooler/setup/wire_spooler.service /etc/systemd/system/wire_spooler.service
 systemctl enable wire_spooler.service
